96ae1bc40f7589c7e9ef18622b4729d8be1bcc4b
hans <hans>
2021-12-06 18:05:22 +0800

    master - Version 0.1H
    Completed :
    1. Creating Excel Files
    2.Creating Workbook
    3.Creating Worksheet
    4.Insert data
    5.Auto Saved to a location

---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Excel_12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application(); //initialize the excel
+
+            if (xlApp == null) 
+            {
+                MessageBox.Show("Excel is not properly installed!!"); //try to check if the excel is installed or not
+                return;
+            }
+
+
+            Excel.Workbook xlWorkBook; //create new workbook
+            Excel.Worksheet xlWorkSheet;//create new worksheet
+            object misValue = System.Reflection.Missing.Value;
+
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            xlWorkSheet.Cells[1, 1] = "ID";
+            xlWorkSheet.Cells[1, 2] = "Name";
+            xlWorkSheet.Cells[2, 1] = "1";
+            xlWorkSheet.Cells[2, 2] = "One";
+            xlWorkSheet.Cells[3, 1] = "2";
+            xlWorkSheet.Cells[3, 2] = "Two";
+
+
+
+            xlWorkBook.SaveAs("C:\\Users\\user\\source\\repos\\Excel 12\\cSharpExcel.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
