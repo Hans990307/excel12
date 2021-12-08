9ed046654fc861be5d36d5c884cd3e3e2cbed2d7
hans <hans>
2021-12-08 16:36:29 +0800

    version 0.3H

---
+++ Browse.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data;
+using System.IO;
+
+namespace Excel_12
+{
+    public class open
+    {
+        public DataTable Openbutton(string Filename, string FileExtension) {
+
+            string constr;
+            string SelectF = "Select * from [$Sheet1A1:end]"; //select data inside the excel
+
+
+
+            if (FileExtension.CompareTo(".xls") == 0) //if compare extension is the same
+                constr = @"Provider=Microsoft.JET.OLEDB.4.0;Data Source=" +
+                            Filename +
+                            ";Extended Properties='Excel 8.0;HDR=YES;';"; //if the extension files is ".xls" this will be the connector path
+            else
+                constr = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" +
+                            Filename +
+                            ";Extended Properties='Excel 8.0;HDR=YES;';"; //if the extension files is ".xlxs" this will be the connector path
+
+            DataTable Data1 = new DataTable(); //create a new data table
+            OleDbConnection con = new OleDbConnection(constr);//Represents an open connection to a data source.
+            OleDbDataAdapter dapter = new OleDbDataAdapter(SelectF, con); //Represents a set of data commands and a database connection that are used to fill the DataSet and update the data source.
+            dapter.Fill(Data1);
+            return Data1;
+
+
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -9,13 +9,14 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
 
 namespace Excel_12
 {
     public partial class Form1 : Form
     {
         classic class1 = new classic();
-        open class2 = new open();
+        open Browse = new open();
         public Form1()
         {
             InitializeComponent();
@@ -34,7 +35,44 @@ namespace Excel_12
 
         private void button2_Click(object sender, EventArgs e)
         {
-            class2.Openbutton();
+   
+            
+            string filepath;
+            string fileext;
+
+
+            OpenFileDialog File1 = new OpenFileDialog(); //open dialog to choose the file
+            if (File1.ShowDialog() == System.Windows.Forms.DialogResult.OK)//if user select a file
+            {
+                filepath = File1.FileName;//get the path of the file
+                fileext = Path.GetExtension(filepath);//get the file extension
+                if (fileext.CompareTo(".xls") == 0 || fileext.CompareTo(".xlxs") == 0)
+                {
+                    try
+                    {
+                        DataTable Data1 = new DataTable();//create datatable
+                        Data1 = Browse.Openbutton(filepath, fileext);//call browse function
+                        dataGridView1.Visible = true;//popout datagridview if true
+                        dataGridView1.DataSource = Data1;//connect datatable with datagridview
+
+                    }
+                    catch
+                    {
+
+                        MessageBox.Show("Error");//show message if got error;
+
+
+                    }
+                }
+
+            }
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
